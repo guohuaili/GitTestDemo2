941e772eb45761229236ff0b1bc41bb2e71ac4a0
lgh <lgh@123.com>
2015-10-21 15:32:37 +0800

    添加 View

---
+++ gitTestDemo/gitTestDemo/View.h
@@ -0,0 +1,13 @@
+//
+//  View.h
+//  gitTestDemo
+//
+//  Created by qianfeng007 on 15/10/21.
+//  Copyright (c) 2015年 LiGuohuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface View : UIView
+
+@end
---
+++ gitTestDemo/gitTestDemo/View.m
@@ -0,0 +1,21 @@
+//
+//  View.m
+//  gitTestDemo
+//
+//  Created by qianfeng007 on 15/10/21.
+//  Copyright (c) 2015年 LiGuohuai. All rights reserved.
+//
+
+#import "View.h"
+
+@implementation View
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
